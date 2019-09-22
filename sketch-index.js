ac165635035e5a4d23a9a4a0e272f438093d0f6d
danielasaldana <53918582+danielasaldana@users.noreply.github.com>
2019-09-22 19:22:29 -0300

    Update sketch-index.js

--- sketch-index.js
+++ sketch-index.js
@@ -1,10 +1,11 @@
-    
+  var r ;
+
 function setup() {
   var canvas = createCanvas(windowWidth,windowHeight);
   canvas.position(0,0);
   canvas.style('z-index', -1);
   colorMode(HSB,360,100,100);
-  var r = random(0,360)
+  r = random(0,360)
   background(r,100,100);
 
 }
@@ -13,7 +14,8 @@ function draw(){
 	inventada(random(1, 100));
 }
 function inventada(diametro){
-	stroke(r,100,100);
+	colorMode(HSB, 360, 100,100);
+        stroke(r,100,75);
 	colorMode(RGB);
 	fill(255,100);
 	ellipse(mouseX, mouseY, diametro, diametro);
